407b75a7507dbfdef4a64d0226e78005decf3e3b
faulkhuang <faulkhuang@gmail.com>
2017-06-22 03:06:44 +0000

    Delete unnesasary .bz2 library.

--- user-side-linux.py
+++ user-side-linux.py
@@ -1,14 +1,14 @@
 # encoding: utf-8
 
-from gi.repository import Gdk
+from gi.repository import gtk
 
 
-window = Gdk.get_default_root_window()
+window = gtk.get_default_root_window()
 x, y, width, height = window.get_geometry()
 
 print("The size of the root window is {} x {}".format(width, height))
 
-pb = Gdk.pixbuf_get_from_window(window, x, y, width, height)
+pb = gtk.pixbuf_get_from_window(window, x, y, width, height)
 
 if pb:
     pb.savev("svreenshot.png", "png", (), ())

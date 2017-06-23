231b6a051229cfdb6ba489a0ccb6d9f41b2258ce
faulkhuang <faulkhuang@hotmail.com>
2017-06-24 04:16:40 +0800

    Modify user-side-windows screenshot() function as one shot.

--- user-side-windows.py
+++ user-side-windows.py
@@ -6,24 +6,25 @@ import os
 from PIL import Image
 from PIL import ImageGrab
 
-bmt_path = 'D:\\bmt'
+bmt_path = 'C:\\bmt'
 
 def ensure_dir(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
+	return
 
 def screenshot():
-	print "Input any key to leave program."
-	while True:
-		time.sleep(1.0)
-		t = time.strftime("%Y_%m_%d") + time.strftime("_%H_%M_%S")
-		img = ImageGrab.grab()
-		img = img.resize((1920,1080))
-		img.save(t+".jpg","JPEG")
-		if msvcrt.kbhit():
-			break    
-			print "Bye Bye!"
+	#print "Input any key to leave program."
+	#while True:
+	#	time.sleep(1.0)
+	t = time.strftime("%Y_%m_%d") + time.strftime("_%H_%M_%S")
+	img = ImageGrab.grab()
+	img = img.resize((1920,1080))
+	img.save(t+".jpg","JPEG")
+	#if msvcrt.kbhit():
+	#	break    
+	#	print "Bye Bye!"
 	return
 	
 ensure_dir(bmt_path)
-#screenshot()
+screenshot()

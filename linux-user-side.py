6106dfe58673737a1c87cd80f06fc77263ca0f65
faulkhuang <faulkhuang@hotmail.com>
2017-06-25 09:08:39 -0700

    Rename .py file and chmod 755 linux.py

--- linux-user-side.py
+++ linux-user-side.py
@@ -11,7 +11,7 @@ print("The size of the root window is {} x {}".format(width, height))
 pb = gtk.pixbuf_get_from_window(window, x, y, width, height)
 
 if pb:
-    pb.savev("svreenshot.png", "png", (), ())
-    print("Screenshot saved to screenshot.png.")
+    pb.savev("svreenshot.jpg", "jpg", (), ())
+    print("Screenshot saved to screenshot.jpg.")
 else:
     print("Unable to get the screenshot.")

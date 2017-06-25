37db54a8fd487d5d2d246521e24592c07fdb7a6b
faulkhuang <faulkhuang@hotmail.com>
2017-06-26 05:27:55 +0800

    windows side recording beta.

--- windows-user-side.py
+++ windows-user-side.py
@@ -7,7 +7,7 @@ from castro import Castro
 from PIL import Image
 from PIL import ImageGrab
 
-bmt_path = 'C:\\bmt'
+bmt_path = 'D:\\30_Python'
 
 def ensure_dir(path):
 	if not os.path.exists(path):
@@ -18,7 +18,7 @@ def screenshot():
 	#print "Input any key to leave program."
 	#while True:
 	#	time.sleep(1.0)
-	os.chdir('C:\\bmt')
+	os.chdir('D:\\30_Python')
 	t = time.strftime("%Y_%m_%d") + time.strftime("_%H_%M_%S")
 	img = ImageGrab.grab()
 	img = img.resize((1920,1080))
@@ -28,15 +28,15 @@ def screenshot():
 	#	print "Bye Bye!"
 	return
 	
-def record():
-	os.chdir('C:\\bmt')
-	c = Castro()
+def record(rtime):
+	os.chdir('D:\\30_Python')
+	t = time.strftime("%Y_%m_%d") + time.strftime("_%H_%M_%S")
+	c = Castro(filename = t + ".swf")
 	c.start()
-	print "Start recording!"
-	c.stop()	
-	return
+	time.sleep(rtime)
+	c.stop()
 	
 if __name__ == "__main__":
 	ensure_dir(bmt_path)
 	#screenshot()
-	record()
+	record(60)
